e0ff2d85ff7054a70b640f9604dcd2d18b226bc0
Daisy <daisy@vizir.com.br>
2021-06-22 09:50:28 -0300

    feat: add event listener on authentication

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
-import { useState } from 'react';
-import { createContext } from 'react'
+import { createContext, useState, useEffect } from 'react'
 import { BrowserRouter, Route } from 'react-router-dom'
 
-import { Home } from "./pages/Home";
-import { NewRoom } from "./pages/NewRoom";
-import { firebase, auth } from './services/firebase';
+import { Home } from "./pages/Home"
+import { NewRoom } from "./pages/NewRoom"
+import { firebase, auth } from './services/firebase'
 
 export const AuthContext = createContext({} as AuthContextType);
 
@@ -21,6 +20,29 @@ type AuthContextType = {
 
 function App() {
   const [user, setUser] = useState<User>()
+
+  // Toda vez que inclui um event listener, precisa se descadastrar dele no final do useEffect
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(user => {
+      if (user) {
+        const { displayName, photoURL, uid } = user
+
+        if (!displayName || !photoURL) {
+          throw new Error('Missing information from Google Account.')
+        }
+
+        setUser({
+          id: uid,
+          name: displayName,
+          avatar: photoURL
+        })
+      }
+    })
+
+    return () => {
+      unsubscribe()
+    }
+  }, [])
 
   async function signInWithGoogle() {
     const provider = new firebase.auth.GoogleAuthProvider()
