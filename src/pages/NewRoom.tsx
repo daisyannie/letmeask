29c6ab99d6c6c7effbbc3db03979b974b4f2580a
Daisy <daisy@vizir.com.br>
2021-06-22 22:03:16 -0300

    feat: save new room on firebase database

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,16 +1,36 @@
+import { useState } from 'react'
+import { FormEvent } from 'react'
 import { Link } from 'react-router-dom'
 
-//import { useAuth } from '../hooks/useAuth'
+import { useAuth } from '../hooks/useAuth'
 
 import illustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg'
 
 import { Button } from '../components/Button'
+import { database } from '../services/firebase'
 
 import '../styles/auth.scss'
 
 export function NewRoom() {
-  //const { user } = useAuth()
+  const { user } = useAuth()
+const [newRoom, setNewRoom] = useState('')
+
+  async function handleCreateRoom(event: FormEvent) {
+    event.preventDefault()
+    
+    if (newRoom.trim() === '') {
+      return
+    }
+
+    const roomRef = database.ref('rooms');
+    
+    const firebaseRoom = await roomRef.push({
+      title: newRoom,
+      authorId: user?.id
+    })
+  }
+
   return (
     <div id="page-auth">
       <aside>
@@ -23,10 +43,12 @@ export function NewRoom() {
           <img src={logoImg} alt="Letmeask"/>
           
           <h2>Criar uma nova sala</h2>
-          <form>
+          <form onSubmit={handleCreateRoom}>
             <input 
               type="text"
               placeholder="Digite o código da sala"
+              onChange={event => setNewRoom(event.target.value)}
+              value={newRoom}
             />
             <Button type="submit">
               Entrar na sala
