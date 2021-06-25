b3ec9fcca3ac7becd8f7767d46c5143b890cbe64
Daisy <daisy@vizir.com.br>
2021-06-24 22:39:50 -0300

    feat: add like button

--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -1,3 +1,4 @@
+import { ReactNode } from 'react'
 import '../styles/question.scss'
 
 type QuestionProps = {
@@ -5,11 +6,12 @@ type QuestionProps = {
   author: {
     name: string
     avatar: string
-  }
+  },
+  children?: ReactNode
 }
 
 export function Question({
-  content, author 
+  content, author, children
 }: QuestionProps) {
   return (
     <div className="question">
@@ -19,7 +21,7 @@ export function Question({
           <img src={author.avatar} alt={author.name}></img>
           <span>{author.name}</span>
         </div>
-        <div></div>
+        <div>{children}</div>
       </footer>
     </div>
   )
