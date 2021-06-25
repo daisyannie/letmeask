29565639d02787050a5579f33b963026879dd354
Daisy <daisy@vizir.com.br>
2021-06-24 21:51:00 -0300

    feat: add question component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -61,7 +61,7 @@ export function Home() {
           <form onSubmit={handleJoinRoom}>
             <input 
               type="text"
-              placeholder="Digite o código da sala"
+              placeholder="Digite o nome da sala"
               onChange={event => setRoomCode(event.target.value)}
               value={roomCode}
             />
