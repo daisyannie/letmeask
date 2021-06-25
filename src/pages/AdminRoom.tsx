9bffefeedc1c67bcbf42c8db394e745e5a8ae777
Daisy <daisy@vizir.com.br>
2021-06-24 23:24:48 -0300

    feat: add delete question functionality

--- src/pages/AdminRoom.tsx
+++ src/pages/AdminRoom.tsx
@@ -1,10 +1,13 @@
-import { useParams } from 'react-router-dom'
+import { useHistory, useParams } from 'react-router-dom'
 
 import logoImg from '../assets/images/logo.svg'
+import deleteImg from '../assets/images/delete.svg'
+
 import { Button } from '../components/Button'
 import { Question } from '../components/Question'
 import { RoomCode } from '../components/RoomCode'
 import { useRoom } from '../hooks/useRoom'
+import { database } from '../services/firebase'
 
 import '../styles/room.scss'
 
@@ -14,7 +17,15 @@ type RoomParams = {
 
 export function AdminRoom() {
   const { id: roomId } = useParams<RoomParams>()
+
   const { title, questions } = useRoom(roomId)
+
+
+  async function handleDeleteQuestion(questionId: string) {
+    if(window.confirm('Tem certeza que deseja excluir esta pergunta?')) {
+      await database.ref(`rooms/${roomId}/questions/${questionId}`).remove();
+    }
+  }
   
   return (
     <div id="page-room">
@@ -23,7 +34,11 @@ export function AdminRoom() {
           <img src={logoImg} alt="Letmeask" />
           <div>
             <RoomCode code={roomId}/>
-            <Button isOutlined>Encerrar sala</Button>
+            <Button 
+              isOutlined
+            >
+              Encerrar sala
+            </Button>
           </div>
         </div>
       </header>
@@ -41,7 +56,14 @@ export function AdminRoom() {
                 key={question.id}
                 content={question.content}
                 author={question.author}
-              />
+              >
+                <button
+                  type="button"
+                  onClick={() => handleDeleteQuestion(question.id)}
+                >
+                  <img src={deleteImg} alt="Remover pergunta"/>
+                </button>
+              </Question>
             )
           })}
         </div>
